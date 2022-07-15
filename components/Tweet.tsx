5b4784bbdc5eed2c59bb5feaf48c7a8275f49fce
yoshi-non <himekawa.yoshino44@gmail.com>
2022-07-15 18:55:53 +0900

    Add Adding Comment

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,8 +1,10 @@
-import { Comment, Tweet } from "../typings"
+import { Comment, CommentBody, Tweet } from "../typings"
 import TimeAgo from "react-timeago"
 import { ChatAlt2Icon, HeartIcon, SwitchHorizontalIcon, UploadIcon } from "@heroicons/react/outline"
 import { fetchComments } from "../utils/fetchComments"
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
+import { useSession } from "next-auth/react"
+import toast from "react-hot-toast"
 
 interface Props {
     tweet: Tweet
@@ -11,6 +13,9 @@ interface Props {
 const Tweet = ({tweet}: Props) => {
 
     const [comments, setComments] = useState<Comment[]>([])
+    const [commentBoxVisible, setCommentBoxVisible] = useState<boolean>(false)
+    const [input, setInput] = useState<string>('')
+    const { data: session } = useSession()
 
     const refreshComments = async () => {
         const comments: Comment[] = await fetchComments(tweet._id)
@@ -21,8 +26,36 @@ const Tweet = ({tweet}: Props) => {
         refreshComments()
     }, [])
 
+    const postComment = async () => {
+        const commentToast = toast.loading('Posting Comment...')
+        const commentInfo: CommentBody = {
+            comment: input,
+            tweetId: tweet._id,
+            username: session?.user?.name || "Unknown User",
+            profileImg: session?.user?.image || "https://links.papareact.com/gll",
+        }
+
+        const result = await fetch(`/api/addComment`, {
+            body: JSON.stringify(commentInfo),
+            method: "POST",
+        })
+
+        toast.success('Comment Posted!', {
+            id: commentToast,
+        })
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        postComment()
+
+        setInput('')
+        setCommentBoxVisible(false)
+        refreshComments()
+    }
+
   return (
-    <div className="flex flex-col space-x-3 border-y border-gray-100 p-5">
+    <div key={tweet._id} className="flex flex-col space-x-3 border-y border-gray-100 p-5">
         <div className="flex space-x-3">
             <img className="h-10 w-10 rounded-full object-cover" src={tweet.profileImg} alt="" />
 
@@ -44,7 +77,7 @@ const Tweet = ({tweet}: Props) => {
             </div>
         </div>
         <div className="mt-5 flex justify-between">
-            <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+            <div onClick={() => session && setCommentBoxVisible(!commentBoxVisible)} className="flex cursor-pointer items-center space-x-3 text-gray-400">
                 <ChatAlt2Icon className="h-5 w-5"/>
                 <p>{comments.length}</p>
             </div>
@@ -60,12 +93,18 @@ const Tweet = ({tweet}: Props) => {
         </div>
 
         {/* Comment Box logic */}
+        {commentBoxVisible && (
+            <form onSubmit={handleSubmit} className="mt-3 flex space-x-3">
+                <input value={input} onChange={(e) => setInput(e.target.value)} className="flex-1 rounded-lg bg-gray-100 p-2 outline-none" type="text" placeholder="Write a comment..." />
+                <button disabled={!input} type="submit" className="text-twitter disabled:text-gray-200">Post</button>
+            </form>
+        )}
 
         {comments?.length > 0 && (
-            <div className="my-2 mt-5 max-h-44 space-y-5 border-t border-gray-100 p-5">
+            <div className="my-2 mt-5 space-y-5 overflow-hidden border-t border-gray-100 p-5">
                 {comments.map((comment) => (
                     <div key={comment._id} className="relative flex space-x-2">
-                        <hr className="absolute left-5 top-10 h-8 border-x border-twitter/30" />
+                        <hr className="absolute left-5 top-10 h-8 border-x border-gray/20" />
                         <img src={comment.profileImg} className="mt-2 h-7 w-7 object-cover rounded-full" alt="" />
                         <div>
                             <div className="flex items-center space-x-1">
